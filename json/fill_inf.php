0751b4865bb5bc9511eac1d20f668fe8aa0a462d
zhengzefanyy <zhengzefanyy@gmail.com>
2015-05-22 13:56:26 +0800

    申请贷款页面逻辑

---
+++ json/fill_inf.php
@@ -0,0 +1,32 @@
+<?php
+//设置页面内容是html编码格式是utf-8
+header("Content-Type: text/plain;charset=utf-8");
+//header("Content-Type: application/json;charset=utf-8");
+//header("Content-Type: text/xml;charset=utf-8");
+//header("Content-Type: text/html;charset=utf-8");
+//header("Content-Type: application/javascript;charset=utf-8");
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    post_form();
+}
+function post_form(){
+            $city = $_POST["city"];
+            $userName = $_POST["userName"];
+            $phone = $_POST["phone"];
+            $check_code = $_POST["reviewCode"];
+            $file1 = $_FILES["file1"];
+            $file2 = $_FILES["file2"];
+            $result = array(
+                "city"=>$city,
+                "phone"=>$phone,
+                "check_code"=>$check_code,
+                "username"=>$userName,
+                "file1"=>$file1,
+                "file2"=>$file2
+            );
+            $array = array("res"=>0,"msg"=>"上传成功","result"=>$result);
+            echo json_encode($array);
+}
+
+?>
