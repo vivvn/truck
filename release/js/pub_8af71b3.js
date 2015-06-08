1015e8104040ad7ace52e9af53a87dc256ca11c3
zhengzefanyy <zhengzefanyy@gmail.com>
2015-06-08 10:28:27 +0800

    对项目进行打包

---
+++ release/js/pub_8af71b3.js
@@ -0,0 +1,1 @@
+var uploadMode=avalon.define({$id:"nav",navData:[{href:"index",val:"首页"},{href:"mortgage",val:"车贷服务"},{href:"trucks",val:"重卡平台"},{href:"transport",val:"运力云"},{href:"affiliate",val:"我要挂靠"},{href:"join",val:"我要加盟"}],active:1});
