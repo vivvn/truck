fe5b339933d1db3c6992608bb50a6fbbba468608
zhengzefanyy <zhengzefanyy@gmail.com>
2015-07-09 11:50:50 +0800

    车贷模块微调

--- js/pf.js
+++ js/pf.js
@@ -1,25 +1,28 @@
 $(function(){
+    $(".addScore").Validform();
     var count = 0;
     $('input[type=radio]').change(function(event){
         var score = $(event.target).data('score');
-        $(event.target).parents('.form-group').data('score',score);
+        $(event.target).parents('.form-horizontal').data('score',score);
         // var fScore=$(event.target).parents('.form-group').data('score');
     })
     $('.sub_btn').click(function(){
-        var array = [];
-        $('.form-group').each(function(){
+        var array = {}
+        $('.form-horizontal').each(function(){
             var score = $(this).data('score');
+            var name = $(this).data('name');
             if(!score){
-                console.log(score);
+                console.log('不存在score');
             }else{
-                array.push(score);
+                array[name] = score;
             }
         })
-        var temp = 0;
-        for(var i=0;i<array.length;i++){
-            temp += array[i];
-        }
-        console.log(temp);
-        alert('分数为：'+temp)
+        console.log(array);
+        // var temp = 0;
+        // for(var i=0;i<array.length;i++){
+        //     temp += array[i];
+        // }
+        // console.log(temp);
+        // alert('分数为：'+temp);
     })
 })
